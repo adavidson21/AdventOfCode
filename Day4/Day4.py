d58b7cca06e9d74bdeae7a8902d6b079aaaa245d
Alexis Oswald <alexisoswald98@gmail.com>
2022-12-05 12:57:25 -0500

    Day 4 part 2

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,5 +1,6 @@
 data = open("Day4/Input.txt").read().split("\n")
-count = 0
+count1 = 0
+count2 = 0
 
 for d in data:
     set = d.split(',')
@@ -11,8 +12,15 @@ for d in data:
 
     # Check min and max of each range with other range.
     if(int(r1[0]) in range2 and int(r1[1]) in range2):
-        count += 1
+        count1 += 1
     elif(int(r2[0]) in range1 and int(r2[1]) in range1):
-        count += 1
+        count1 += 1
 
-print("Part 1: ", count)
+    # Check min and max of each range with other range.
+    if(int(r1[0]) in range2 or int(r1[1]) in range2):
+        count2 += 1
+    elif(int(r2[0]) in range1 or int(r2[1]) in range1):
+        count2 += 1
+
+print("Part 1: ", count1)
+print("Part 2: ", count2)
