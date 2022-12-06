748304ceb1c3616fde8f2f7aae2c4b733747c5d9
Alexis Oswald <alexisoswald98@gmail.com>
2022-12-06 12:59:17 -0500

    day 6 part 2

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -2,8 +2,18 @@ data = open("Day6/Input.txt").read()  # parse by char
 
 # start of packet marker = sequence of 4 unique characters
 for i in range(0, len(data)-4):
-    first, second, third, fourth = data[i], data[i+1], data[i+2], data[i+3]
-    # Ugly brute force check if any match
-    if(first != second and first != third and first != fourth and second != third and second != fourth and third != fourth):
+    window = set()
+    for j in range(i, i+4):
+        window.add(data[j])
+    if(len(window) == 4):
         print("Part 1: ", i+4)
+        break
+
+# start of packet marker = sequence of 14 unique characters
+for i in range(0, len(data)-14):
+    window = set()
+    for j in range(i, i+14):
+        window.add(data[j])
+    if(len(window) == 14):
+        print("Part 2: ", i+14)
         break
